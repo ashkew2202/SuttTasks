705292b41956134e6bdad744fc98931afd825912
myst1cboomer <anshpmk@hotmail.com>
2025-03-10 13:28:37 +0530

    Polls app is made although it could have been better

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,25 +3,6 @@ from .models import Question, Choice
 from django.template import loader
 from django.http import Http404, HttpResponseRedirect
 from django.urls import reverse
-from django.views import generic
-
-class IndexView(generic.ListView):
-    template_name = 'polls/index.html'
-    context_object_name = 'latest_question_list'
-
-    def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('pub_date')
-
-
-class DetailView(generic.DetailView):
-    model = Question
-    template_name = 'polls/detail.html'
-
-
-class ResultsView(generic.DetailView):
-    model = Question
-    template_name = 'polls/results.html'
 
 def detail(request, question_id):
     try:
@@ -32,7 +13,10 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    question = get_object_or_404(Question, id=question_id)
+    try :
+        question = Question.objects.get(id=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
     return render(request, 'polls/results.html', {'question': question})
 
 
@@ -52,7 +36,7 @@ def vote(request, question_id):
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
-        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+        return HttpResponseRedirect(reverse('results', args=(question.id,)))
     
 def index(request):
     latest_question_list = Question.objects.order_by("pub_date")
